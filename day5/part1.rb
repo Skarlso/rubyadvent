9131481bce9693806f19c25937d558331e82a167
Gergely Brautigam <skarlso777@gmail.com>
2016-03-06 08:55:15 -0500

    Added the three kings.

--- day5/part1.rb
+++ day5/part1.rb
@@ -5,7 +5,13 @@ def three_vowels(name)
 end
 
 def double_trouble(name)
+  return name =~ /(.)\1+/
 end
 
 def no_candy(name)
+  return name =~ /^(?:(?!ab|cd|pq|xy).)+$/
 end
+
+puts double_trouble 'hgjeekdkai'
+puts three_vowels 'hgjekdka'
+puts no_candy 'dfgabdf'
