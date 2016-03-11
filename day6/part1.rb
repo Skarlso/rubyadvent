8cf3e0d3383a8d7578e260b95dbe438eec0143a0
Gergely Brautigam <skarlso777@gmail.com>
2016-03-11 11:59:46 -0500

    Progress.

--- day6/part1.rb
+++ day6/part1.rb
@@ -6,7 +6,15 @@ grid = Array.new
   grid[x] = Array.new(1000, false)
 end
 
-File.readlines(file).each do |line|
+File.readlines('input.txt').each do |line|
   split = line.split(' ')
+  if split[0] == "toggle"
+    fromx, fromy = split[1].split(',')
+    action = "toggle"
+  else
+    fromx, fromy = split[2].split(',')
+    action = split[1]
+  end
+
   
 end
