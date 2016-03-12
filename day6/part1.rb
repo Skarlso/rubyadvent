1ccf54948dce373a34a33195581475a8a6c2b733
Gergely Brautigam <skarlso777@gmail.com>
2016-03-11 22:48:03 -0500

    Solution for day 6 first draft which works correctly.

--- day6/part1.rb
+++ day6/part1.rb
@@ -9,12 +9,27 @@ end
 File.readlines('input.txt').each do |line|
   split = line.split(' ')
   if split[0] == "toggle"
-    fromx, fromy = split[1].split(',')
+    fromx, fromy = split[1].split(',').map(&:to_i)
     action = "toggle"
   else
-    fromx, fromy = split[2].split(',')
+    fromx, fromy = split[2].split(',').map(&:to_i)
     action = split[1]
   end
 
-  
+  tox, toy = split.last.split(',').map(&:to_i)
+
+  fromx.upto(tox) do |x|
+    fromy.upto(toy) do |y|
+      case action
+      when "toggle"
+        grid[x][y] = !grid[x][y]
+      when "on"
+        grid[x][y] = true
+      when "off"
+        grid[x][y] = false
+      end
+    end
+  end
 end
+
+puts grid.flatten.count{ |x| x == true }
