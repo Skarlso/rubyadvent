d0c8cf0505c752eadd8ae36ddf76a49e0f19163d
Gergely Brautigam <skarlso777@gmail.com>
2016-04-13 07:40:17 +0200

    Parsing...

--- day7/part1.rb
+++ day7/part1.rb
@@ -1,7 +1,32 @@
 #!/usr/bin/env ruby
 
-operations = []
+operations = {}
 
 File.readlines('input.txt').each do |line|
-  operations << line
+  case line.chomp
+  when /(\w+) OR (\w+) -> (\w+)/
+    then operations[Regexp.last_match(3)] =
+           "#{Regexp.last_match(1)}|#{Regexp.last_match(2)}"
+  when /(\d+) AND (\w+) -> (\w+)/
+    then operations[Regexp.last_match(3)] =
+           "#{Regexp.last_match(1)}&#{Regexp.last_match(2)}"
+  when /(\w+) AND (\w+) -> (\w+)/
+    then operations[Regexp.last_match(3)] =
+           "#{Regexp.last_match(1)}&#{Regexp.last_match(2)}"
+  when /(\w+) LSHIFT (\d+) -> (\w+)/
+    then operations[Regexp.last_match(3)] =
+           "#{Regexp.last_match(1)}<<#{Regexp.last_match(2)}"
+  when /(\w+) RSHIFT (\d+) -> (\w+)/
+    then operations[Regexp.last_match(3)] =
+           "#{Regexp.last_match(1)}>>#{Regexp.last_match(2)}"
+  when /NOT (\w+) -> (\w+)/
+    then operations[Regexp.last_match(2)] =
+           "~#{Regexp.last_match(1)}"
+  when /(\d+) -> (\w+)/
+    then operations[Regexp.last_match(2)] =
+           "#{Regexp.last_match(1)}=#{Regexp.last_match(2)}"
+  when /(\w+) -> (\w+)/
+    then operations[Regexp.last_match(2)] =
+           "#{Regexp.last_match(1)}=#{Regexp.last_match(2)}"
+  end
 end
